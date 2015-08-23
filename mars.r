ffa7f1d141c02cf89e738f9e67abbdc5fe35ab75
Reetika Roy <royritu21@yahoo.co.in>
2015-08-23 15:40:23 +0530

    Example to learn MARS from different tutorials

---
+++ mars.r
@@ -0,0 +1,15 @@
+library(earth)
+# load data
+data(longley)
+# fit model
+fit <- earth(Employed~., longley)
+# summarize the fit
+summary(fit)
+# summarize the importance of input variables
+evimp(fit)
+# make predictions
+predictions <- predict(fit, longley)
+# summarize accuracy
+rmse <- mean((longley$Employed - predictions)^2)
+print(rmse)
+
